1a6da276ceeb68293680ddcbb059f4b6c57554bc
Dorin <doriodavid@gmail.com>
2021-03-12 10:35:47 +0100

    setup checkout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout'
+
+//task: load the checkout summary only when continue is clicked
 
 class App extends Component {
   render(){
@@ -8,6 +11,7 @@ class App extends Component {
       <div>
          <Layout>
            <BurgerBuilder />
+           <Checkout />
          </Layout>
          </div>
     );
